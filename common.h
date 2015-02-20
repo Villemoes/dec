36096b4e5fadc482c403519b75c7604d03698f15
Rasmus Villemoes <rv@rasmusvillemoes.dk>
2015-02-20 10:02:09 +0100

    Use cpu_to_le16 for initializing the table
    This would need to be done sooner or later, so might as well do it
    now. I won't be able to do any actual tests on a big-endian machine,
    but at least the table should now be ok on all machines.

--- common.h
+++ common.h
@@ -2,6 +2,14 @@
 #define COMMON_H
 
 #include <stdint.h>
+#include <endian.h>
+
+/* glibc's htole16 doesn't work in initializers, so we roll our own. */
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+#define cpu_to_le16(x) (x)
+#else
+#define cpu_to_le16(x) ((((x) & 0xff) << 8) | (((x) >> 8) & 0xff))
+#endif
 
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
 
